3721ab617d6f3281afb165a59ca50aed0c46964b
Papish Limbu <lpapish97@gmail.com>
2024-12-31 00:29:04 +0545

    basic user register added

--- application/apps/backend/src/api/auth.ts
+++ application/apps/backend/src/api/auth.ts
@@ -1,12 +1,49 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import db from "../lib/prisma";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 const authRoutes = express.Router();
 
-authRoutes.get("/login", (req, res) => {
-  res.status(200).json({ token: "fake-jwt-token" });
+const JWT_SECRET = process.env.PORT || "";
+
+authRoutes.get("/login", async (req, res) => {
+  const users = await db.user.findMany();
+  res.status(200).json({ users });
 });
 
-authRoutes.post("/register", (req, res) => {
-  res.status(200).json({ token: "fake-jwt-token" });
-});
+authRoutes.post(
+  "/register",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, email, password } = req.body;
+
+      const existingUser = await db.user.findUnique({ where: { email } });
+
+      if (existingUser) {
+        res.status(409).json({ statusCode: 409, message: "Email is already taken" });
+        return;
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = await db.user.create({
+        data: {
+          email,
+          name,
+          password: hashedPassword,
+        },
+      });
+
+      const access_token = jwt.sign({ userId: user.id }, JWT_SECRET, {
+        expiresIn: "1xxxxh",
+      });
+
+      res.status(200).json({ message: "Login successful", token: access_token });
+    } catch (err) {
+      next(err);
+    }
+  },
+);
 
 export { authRoutes };
--- application/apps/backend/src/app.ts
+++ application/apps/backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 import dotenv from "dotenv";
 import swaggerDoc from "./swagger/swagger-output.json";
@@ -11,5 +11,16 @@ app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use("/api/auth", authRoutes);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = err.statusCode || 500;
+
+  process.env.NODE_ENV === "development" ? console.error(err.stack) : "";
+  
+  res.status(statusCode).json({
+    statusCode,
+    message: "Internal Server Error",
+  });
+});
 
 export default app;
